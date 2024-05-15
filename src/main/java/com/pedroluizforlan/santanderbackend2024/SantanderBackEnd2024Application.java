4ff601dd7b892611c9a1bf8d5cf0bd087696e1de
Pedro Luiz Forlan de Paula <pedro.forlan2000@gmail.com>
2024-05-15 08:01:25 -0300

    Initial commit

---
+++ src/main/java/com/pedroluizforlan/santanderbackend2024/SantanderBackEnd2024Application.java
@@ -0,0 +1,13 @@
+package com.pedroluizforlan.santanderbackend2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderBackEnd2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderBackEnd2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedroluizforlan/santanderbackend2024/SantanderBackEnd2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedroluizforlan.santanderbackend2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderBackEnd2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
